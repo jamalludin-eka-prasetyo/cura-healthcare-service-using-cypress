0eaa1e654e0b336479f25cbbcdb86cb8df91cd71
Jamalludin Eka Prasetyo <jamalludin.eka.prasetyo@gmail.com>
2026-01-05 22:56:07 +0700

    add dynamic data for all login page

--- cypress/e2e/dynamic_login_test.cy.js
+++ cypress/e2e/dynamic_login_test.cy.js
@@ -1,5 +1,6 @@
+// Login dynamic with 1 testcase for all user
 // describe('Dynamic Test Login', () => {
-//   it('Runs login from CSV data using username : ${user.username}', () => {
+//   it('Runs login from CSV data', () => {
 //     cy.readCSV('cypress/fixtures/Login_data.csv').then((users) => {
 //       users.forEach((user) => {
 //             //Login
@@ -24,15 +25,27 @@
 //   });
 // });
 
+
+// login dynamic for 1 user 1 test case
 const users = Cypress.env('users');
 
 describe('Dynamic Test Login', () => {
   users.forEach((user) => {
-    it(`Login test for ${user.username}`, () => {
-      cy.visit('/profile.php#login')
-      cy.get('#txt-username').type(user.username)
-      cy.get('#txt-password').type(user.password)
-      cy.get('#btn-login').click()
+    it(`Login test for user :  ${user.username}`, () => {
+        cy.visit('/profile.php#login')
+        cy.get('#txt-username').type(user.username)
+        cy.get('#txt-password').type(user.password)
+        cy.get('#btn-login').click()
+        cy.get('body').then(($body) => {
+          if($body.text().includes('Login failed!')) {
+              cy.get('.text-danger').should('contain', 'Login failed! Please ensure the username and password are valid.')
+              cy.url().should('include','/profile.php#login') 
+              cy.log('login gagal menggunakan username', user.username , 'dan password', user.password) 
+          } else {
+              cy.url().should('include','#appointment')
+              cy.log('login berhasil menggunakan username: ', user.username , 'dan password: ', user.password) 
+          }
+        })
     });
   });
 });
--- cypress/e2e/hamburger_menu.cy.js
+++ cypress/e2e/hamburger_menu.cy.js
@@ -1,3 +1,4 @@
+const users = Cypress.env('users');
 describe('Hamburger Menu', () => {
     beforeEach(() => {
         cy.visit('/')
@@ -11,32 +12,23 @@ describe('Hamburger Menu', () => {
         cy.url('include','/')
     })
 
-    it('Login from Hamburger Menu', () => {
-        cy.readCSV('cypress/fixtures/Login_data.csv').then((users) => {
-            users.forEach((user) => {
-                cy.get('#menu-toggle').click()
-                cy.get('.sidebar-nav > li:nth-child(4) > a').should('contain','Login').click()
-                cy.url('include','/profile.php#login')
-                
-                cy.get('h2').should('contain','Login')
-        
-                cy.get('#txt-username').type(user.username)
-                cy.get('#txt-password').type(user.password)
-                cy.get('#btn-login').click()
-                
-                cy.get('body').then(($body) => { 
-                    if ($body.text().includes('Login failed!')) { 
-                        cy.get('.text-danger').should('contain', 'Login failed! Please ensure the username and password are valid.')
-                        cy.url().should('include','/profile.php#login') 
-                        cy.log('login gagal menggunakan username', user.username , 'dan password', user.password) 
-                    } else { 
-                        cy.url().should('include','#appointment')
-                        cy.log('login berhasil menggunakan username: ', user.username , 'dan password: ', user.password) 
-                    } 
-                })
-            })
+    
+    users.forEach((user) => {
+    it(`Login test for user :  ${user.username}`, () => {
+        cy.visit('/profile.php#login')
+        cy.get('#txt-username').type(user.username)
+        cy.get('#txt-password').type(user.password)
+        cy.get('#btn-login').click()
+        cy.get('body').then(($body) => {
+          if($body.text().includes('Login failed!')) {
+              cy.get('.text-danger').should('contain', 'Login failed! Please ensure the username and password are valid.')
+              cy.url().should('include','/profile.php#login') 
+              cy.log('login gagal menggunakan username', user.username , 'dan password', user.password) 
+          } else {
+              cy.url().should('include','#appointment')
+              cy.log('login berhasil menggunakan username: ', user.username , 'dan password: ', user.password) 
+          }
         })
-
-
-    })
+    });
+  });
 })
